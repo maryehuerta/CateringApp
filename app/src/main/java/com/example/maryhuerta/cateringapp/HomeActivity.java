d92948a5d072f40121dc94b1777447827646b0d8
maryehuerta <mary.huerta@mavs.uta.edu>
2018-03-23 09:44:16 -0500

    Okay I swear last mergy commit

--- app/src/main/java/com/example/maryhuerta/cateringapp/HomeActivity.java
+++ app/src/main/java/com/example/maryhuerta/cateringapp/HomeActivity.java
@@ -8,12 +8,8 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-<<<<<<< HEAD
-        setContentView(R.layout.activity_home);
 
-=======
         //setContentView(R.layout.activity_home);
         setContentView(R.layout.registration_screen);
->>>>>>> befd768d341299cb4e1b3a08c17b26d7ceee6d05
     }
 }
