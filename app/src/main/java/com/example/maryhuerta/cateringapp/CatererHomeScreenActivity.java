e957ac5c6e2e3941eca65af20c2b77c318c302ad
Miguel <miguel.gomezdefranco@mavs.uta.edu>
2018-03-24 22:04:04 -0500

    All Home Screens
    user, staff and caterer home screens complete and ready to add functionalities

---
+++ app/src/main/java/com/example/maryhuerta/cateringapp/CatererHomeScreenActivity.java
@@ -0,0 +1,17 @@
+package com.example.maryhuerta.cateringapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Miguel Gomez on 3/24/2018.
+ */
+
+public class CatererHomeScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.caterer_home_screen);
+    }
+}
