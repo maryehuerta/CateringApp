0d7a4f3f511c27e9d19f5e5fad567da380b869c8
AMARACHI\amarachi <iwuhamarachi1@gmail.com>
2018-03-25 15:43:34 -0500

    aaded user event details screen for a selected event

---
+++ app/src/main/java/com/example/maryhuerta/cateringapp/SelectedUserEventDetails.java
@@ -0,0 +1,13 @@
+package com.example.maryhuerta.cateringapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelectedUserEventDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selected_user_event_details);
+    }
+}
