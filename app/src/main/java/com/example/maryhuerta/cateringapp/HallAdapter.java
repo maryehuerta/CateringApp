5bd1411190af5e85697af26e88d329942955974d
Cameron <jimmy12201@gmail.com>
2018-03-25 00:15:29 -0500

    Added some GUI's
    Close enough RecyclerView

---
+++ app/src/main/java/com/example/maryhuerta/cateringapp/HallAdapter.java
@@ -0,0 +1,60 @@
+package com.example.maryhuerta.cateringapp;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ListIterator;
+import java.util.Vector;
+
+/**
+ * Created by jimmy on 3/24/2018.
+ */
+
+public class HallAdapter extends RecyclerView.Adapter<HallAdapter.ViewHolder>{
+
+    Vector <HallItem> hallList;
+    private Context context;
+
+    public HallAdapter(Vector<HallItem> hallList, Context context) {
+        this.hallList = hallList;
+        this.context = context;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.hall_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        HallItem currentHall = hallList.get(position);
+
+        holder.TextViewHall.setText(currentHall.getHall());
+        holder.TextViewCapacity.setText(currentHall.getCapactiy());
+        holder.TextViewTime.setText(currentHall.getTime());
+    }
+
+    @Override
+    public int getItemCount() {
+        return hallList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView TextViewHall, TextViewCapacity, TextViewTime;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            TextViewHall = (TextView) itemView.findViewById(R.id.HallText);
+            TextViewCapacity = (TextView) itemView.findViewById(R.id.CapacityText);
+            TextViewTime = (TextView) itemView.findViewById(R.id.TimeText);
+
+        }
+    }
+}
--- app/src/main/java/com/example/maryhuerta/cateringapp/RegistrationScreenActivity.java
+++ app/src/main/java/com/example/maryhuerta/cateringapp/RegistrationScreenActivity.java
@@ -2,6 +2,10 @@ package com.example.maryhuerta.cateringapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.Vector;
 
 /**
  * Created by Miguel Gomez on 3/24/2018.
@@ -12,6 +16,34 @@ public class RegistrationScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.registration_screen);
+        setContentView(R.layout.available_halls);
+        if (true) {
+            populateHallsTest();
+        }
+    }
+
+    private RecyclerView HallRecyclerView;
+    private RecyclerView.Adapter adapter;
+    Vector<HallItem> hallList;
+
+    public void populateHallsTest() {
+        HallRecyclerView = (RecyclerView) findViewById(R.id.HallRecyclerView);
+        HallRecyclerView.setHasFixedSize(true);
+        HallRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        hallList = new Vector<>();
+        for (int i = 0; i < 10; i++) {
+            hallList.add(new HallItem("Hall: " + i, "Capacity: " + i * 100, "10:00"));
+        }
+        for (int i = 0; i < 10; i++) {
+            hallList.add(new HallItem("Hall: " + i, "Capacity: " + i * 100,"10:30"));
+        }
+        for (int i = 0; i < 10; i++) {
+            hallList.add(new HallItem("Hall: " + i, "Capacity: " + i * 100, "11:00"));
+        }
+
+        adapter = new HallAdapter(hallList, this);
+        HallRecyclerView.setAdapter(adapter);
+
     }
 }
