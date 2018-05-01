8286cd0d74344b591846679cf6e83b12a3feef70
Miguel <miguel.gomezdefranco@mavs.uta.edu>
2018-04-30 21:02:38 -0500

    user pulls only his events from the DB

--- app/src/main/java/com/example/maryhuerta/cateringapp/UserEventSummaryScreenActivity.java
+++ app/src/main/java/com/example/maryhuerta/cateringapp/UserEventSummaryScreenActivity.java
@@ -17,10 +17,20 @@ public class UserEventSummaryScreenActivity extends AppCompatActivity implements
     public final int SHOW_DETAIL = 1;
     TextView deleteLater;
     UserModel user;
+
+    String UserID, UserType;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_event_summary_screen);
+
+        /**/
+        String [] UserInfo = getIntent().getStringExtra("USERINFO").split(";");
+        UserID= UserInfo[0];
+        UserType = UserInfo[1];
+        /**/
+
 
         // Called in onCreate function gives random test data to work with
         populateuserRequestedEventsTest();
@@ -41,7 +51,10 @@ public class UserEventSummaryScreenActivity extends AppCompatActivity implements
         DBManager handler = new DBManager(UserEventSummaryScreenActivity.this);
         eventList.clear();
         for (EventModel model: handler.getAllEvents()){
-            eventList.add(new UserRequestedEventItem(model.getLastName(),model.getFirstName(),model.getDate(),model.getTimeOfEvent(), model.getDuration(),model.getHallName(),model.getAttendees(),model.getEventName(), model.getFoodType(), model.getMealType(), model.getFormality(), "DrinkType", model.getSpecialItems(), model.getReserved()));
+            System.out.println("THE USER IS: "+ UserID);
+            if (UserID.equals(model.getUserID())) {
+                eventList.add(new UserRequestedEventItem(model.getLastName(), model.getFirstName(), model.getDate(), model.getTimeOfEvent(), model.getDuration(), model.getHallName(), model.getAttendees(), model.getEventName(), model.getFoodType(), model.getMealType(), model.getFormality(), "DrinkType", model.getSpecialItems(), model.getReserved()));
+            }
         }
         Log.d("event list", eventList.toString());
         adapter = new UserRequestedEventsAdapter(eventList, this, this);
